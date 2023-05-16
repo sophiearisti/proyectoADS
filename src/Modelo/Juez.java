2235a496aa45c521f4fadff2b4b00fc85454bdb9
stillIsaac <isaacjanica@gmail.com>
2023-05-16 07:03:28 -0500

    Ahora si, el anterior commit no lo tenía xd

--- src/Modelo/Juez.java
+++ src/Modelo/Juez.java
@@ -1,5 +1,40 @@
 package Modelo;
 
 public class Juez {
+	private String Nombre;
+	private Integer Codigo;
+	private String ColegioArbitral;
+	
+	public Juez(String nombre, Integer codigo, String colegioArbitral) {
+		super();
+		this.Nombre = nombre;
+		this.Codigo = codigo;
+		this.ColegioArbitral = colegioArbitral;
+	}
 
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+	public Integer getCodigo() {
+		return Codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		Codigo = codigo;
+	}
+
+	public String getColegioArbitral() {
+		return ColegioArbitral;
+	}
+
+	public void setColegioArbitral(String colegioArbitral) {
+		ColegioArbitral = colegioArbitral;
+	}
+	
+	
 }
