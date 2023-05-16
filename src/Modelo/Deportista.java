5b71d5a7d06f50aa869ef32a1a198a5a21b6d058
stillIsaac <isaacjanica@gmail.com>
2023-05-16 07:06:21 -0500

    Al parecer no había hecho el anterior commit tho

--- src/Modelo/Deportista.java
+++ src/Modelo/Deportista.java
@@ -1,5 +1,92 @@
 package Modelo;
 
 public class Deportista {
+	private Integer NoInscripcion;
+	private String Nombre;
+	private Boolean AsistenciaPrimeraVez;
+	private Float Estatura;
+	private String Medalla;
+	private Boolean Abanderado;
+	private String DelegacionPertenece;
+	private Boolean Eliminado;
+	
+	public Deportista(Integer noInscripcion, String nombre, Boolean asistenciaPrimeraVez, Float estatura,
+			String medalla, Boolean abanderado, String delegacionPertenece, Boolean eliminado) {
+		super();
+		this.NoInscripcion = noInscripcion;
+		this.Nombre = nombre;
+		this.AsistenciaPrimeraVez = asistenciaPrimeraVez;
+		this.Estatura = estatura;
+		this.Medalla = medalla;
+		this.Abanderado = abanderado;
+		this.DelegacionPertenece = delegacionPertenece;
+		this.Eliminado = eliminado;
+	}
+
+	public Integer getNoInscripcion() {
+		return NoInscripcion;
+	}
+
+	public void setNoInscripcion(Integer noInscripcion) {
+		NoInscripcion = noInscripcion;
+	}
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+	public Boolean getAsistenciaPrimeraVez() {
+		return AsistenciaPrimeraVez;
+	}
+
+	public void setAsistenciaPrimeraVez(Boolean asistenciaPrimeraVez) {
+		AsistenciaPrimeraVez = asistenciaPrimeraVez;
+	}
+
+	public Float getEstatura() {
+		return Estatura;
+	}
+
+	public void setEstatura(Float estatura) {
+		Estatura = estatura;
+	}
+
+	public String getMedalla() {
+		return Medalla;
+	}
+
+	public void setMedalla(String medalla) {
+		Medalla = medalla;
+	}
+
+	public Boolean getAbanderado() {
+		return Abanderado;
+	}
+
+	public void setAbanderado(Boolean abanderado) {
+		Abanderado = abanderado;
+	}
+
+	public String getDelegacionPertenece() {
+		return DelegacionPertenece;
+	}
+
+	public void setDelegacionPertenece(String delegacionPertenece) {
+		DelegacionPertenece = delegacionPertenece;
+	}
+
+	public Boolean getEliminado() {
+		return Eliminado;
+	}
+
+	public void setEliminado(Boolean eliminado) {
+		Eliminado = eliminado;
+	}
+	
+	
 
 }
--- src/Modelo/Resultado.java
+++ src/Modelo/Resultado.java
@@ -2,11 +2,63 @@ package Modelo;
 
 public class Resultado {
 	
-	private Categoria categoria;
+	private Categoria Categoria;
+	private Juez Juez;
+	private Integer CodigoOro;
+	private Integer CodigoPlata;
+	private Integer CodigoBronce;
 	
+	public Resultado(Modelo.Categoria categoria, Modelo.Juez juez, Integer codigoOro, Integer codigoPlata,
+			Integer codigoBronce) {
+		super();
+		this.Categoria = categoria;
+		this.Juez = juez;
+		this.CodigoOro = codigoOro;
+		this.CodigoPlata = codigoPlata;
+		this.CodigoBronce = codigoBronce;
+	}
 
+	public Categoria getCategoria() {
+		return Categoria;
+	}
 
+	public void setCategoria(Categoria categoria) {
+		Categoria = categoria;
+	}
 
+	public Juez getJuez() {
+		return Juez;
+	}
+
+	public void setJuez(Juez juez) {
+		Juez = juez;
+	}
+
+	public Integer getCodigoOro() {
+		return CodigoOro;
+	}
+
+	public void setCodigoOro(Integer codigoOro) {
+		CodigoOro = codigoOro;
+	}
+
+	public Integer getCodigoPlata() {
+		return CodigoPlata;
+	}
+
+	public void setCodigoPlata(Integer codigoPlata) {
+		CodigoPlata = codigoPlata;
+	}
+
+	public Integer getCodigoBronce() {
+		return CodigoBronce;
+	}
+
+	public void setCodigoBronce(Integer codigoBronce) {
+		CodigoBronce = codigoBronce;
+	}
+	
+	
 	
 
 }
