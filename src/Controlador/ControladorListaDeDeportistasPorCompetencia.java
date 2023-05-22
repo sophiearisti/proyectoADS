3d4b3c60f0a5c0e59c550c96155f8e855b55070b
Camilodez <hernandezz_camilo@javeriana.edu.co>
2023-05-21 19:39:05 -0500

    Cree el controlador de mi pantalla

---
+++ src/Controlador/ControladorListaDeDeportistasPorCompetencia.java
@@ -0,0 +1,31 @@
+package Controlador;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class ControladorListaDeDeportistasPorCompetencia {
+
+	    @FXML
+	    private Button BAtras;
+
+	    @FXML
+	    private TableColumn<?, ?> ColumnFacultad;
+
+	    @FXML
+	    private TableColumn<?, ?> ColumnNoInscripcion;
+
+	    @FXML
+	    private TableColumn<?, ?> ColumnNombre;
+
+	    @FXML
+	    private ComboBox<?> ComboBox;
+
+	    @FXML
+	    private TableView<?> TablaDeportistas;
+
+	
+	    
+
+}
